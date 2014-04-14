8138456673c8cf1360a353f4c2dfae5ea13664dd
Steingrim Dovland <steingrim.dovland@gmail.com>
2014-04-14 23:08:06 +0200

    Initial commit: Empty Java project

---
+++ src/main/java/com/github/steingrd/fermonitor/app/StartFermonitorApp.java
@@ -0,0 +1,14 @@
+package com.github.steingrd.fermonitor.app;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class StartFermonitorApp {
+
+	static final Logger log = LoggerFactory.getLogger(StartFermonitorApp.class);
+	
+	public static void main(String...args) {
+		log.info("Starting FermonitorApp...");
+	}
+	
+}
