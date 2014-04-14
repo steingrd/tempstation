8525098d8b86dc4408f2fbaf2551aa96ebed7325
Steingrim Dovland <steingrim.dovland@gmail.com>
2014-04-14 23:11:38 +0200

    Added basic Vertx http server

--- src/main/java/com/github/steingrd/fermonitor/app/StartFermonitorApp.java
+++ src/main/java/com/github/steingrd/fermonitor/app/StartFermonitorApp.java
@@ -1,14 +1,44 @@
 package com.github.steingrd.fermonitor.app;
 
+import java.util.concurrent.CountDownLatch;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.vertx.java.core.Vertx;
+import org.vertx.java.core.VertxFactory;
+import org.vertx.java.core.http.HttpServer;
+
+import static com.github.steingrd.fermonitor.app.EnvironmentUtils.propertyOrEnvVariableWithDefault;
 
 public class StartFermonitorApp {
 
 	static final Logger log = LoggerFactory.getLogger(StartFermonitorApp.class);
 	
-	public static void main(String...args) {
+	public static void main(String...args) throws InterruptedException {
 		log.info("Starting FermonitorApp...");
+		
+		final Vertx vertx = VertxFactory.newVertx();
+		final HttpServer server = vertx.createHttpServer();
+
+		server.requestHandler(request -> {
+			log.info("Handling request {}", request.path());
+			request.response().end();
+		});
+		
+		server.listen(port(), asyncResult -> {
+			if (asyncResult.succeeded()) {
+				log.info("HTTP server is listening.");
+			} else {
+				throw new RuntimeException("Failed to start HTTP server!");
+			}
+		});
+
+		// block the main thread
+		new CountDownLatch(1).await();
+	}
+	
+	static int port() {
+		return Integer.parseInt(propertyOrEnvVariableWithDefault("PORT", "9090"));
 	}
 	
 }
