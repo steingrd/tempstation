c018e86e71e33be4b672d720d219c72d6cd0f863
Steingrim Dovland <steingrim.dovland@gmail.com>
2014-04-15 10:17:44 +0200

    Empty CreateKey command

---
+++ src/main/java/com/github/steingrd/fermonitor/commands/CreateKey.java
@@ -0,0 +1,11 @@
+package com.github.steingrd.fermonitor.commands;
+
+public class CreateKey {
+
+	public static void main(String[] args) {
+		System.out.println("Creating auth key...");
+		
+		
+	}
+	
+}
