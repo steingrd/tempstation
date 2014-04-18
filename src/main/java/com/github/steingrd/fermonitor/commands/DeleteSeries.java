22d1cfec33354ded87a8405d8952a07cde1a8e71
Steingrim Dovland <steingrim.dovland@gmail.com>
2014-04-18 12:02:47 +0200

    Added deleteseries.sh script

---
+++ src/main/java/com/github/steingrd/fermonitor/commands/DeleteSeries.java
@@ -0,0 +1,53 @@
+package com.github.steingrd.fermonitor.commands;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+
+import com.github.steingrd.fermonitor.app.JedisPoolFactory;
+import com.github.steingrd.fermonitor.app.TempoDbFactory;
+import com.tempodb.client.Client;
+import com.tempodb.models.Filter;
+
+import static com.github.steingrd.fermonitor.app.Configuration.get;
+
+public class DeleteSeries {
+
+	final static Logger log = LoggerFactory.getLogger(DeleteSeries.class);
+
+	public static void main(String[] args) {
+		try {
+			deleteSeries(args);
+		} catch (Exception e) {
+			log.error("deleteSeries failed!", e);
+		}
+	}
+	
+	static void deleteSeries(String[] args) throws Exception {
+		if (args.length != 1) {
+			log.info("Missing args: " + args.length);
+			return;
+		}
+		
+		final String brewsSet = get("FERMONITOR_BREWS_LIST");
+		final String brewId = args[0];
+		
+		final JedisPool jedisPool = new JedisPoolFactory().create();
+		final Client tempodb = new TempoDbFactory().create();
+		
+		try (Jedis jedis = jedisPool.getResource()) {
+			jedis.srem(brewsSet, brewId);
+			jedis.del(brewId + ".lastUpdated");
+			jedis.del(brewId + ".secret");
+			jedisPool.returnResource(jedis);			
+		}
+		
+		Filter filter = new Filter();
+		filter.addKey(brewId);
+		tempodb.deleteSeries(filter);
+		
+	}
+
+}
